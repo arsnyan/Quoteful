2501511a6f441c0f78d5e303310092668af7ee04
arsnyan <arsnyan.dev@gmail.com>
2025-09-18 20:24:25 +0300

    Initial Commit

---
+++ Quoteful/QuotefulApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotefulApp.swift
+//  Quoteful
+//
+//  Created by Арсен Саруханян on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuotefulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuotefulTests/QuotefulTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuotefulTests.swift
+//  QuotefulTests
+//
+//  Created by Арсен Саруханян on 18.09.2025.
+//
+
+import Testing
+@testable import Quoteful
+
+struct QuotefulTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
